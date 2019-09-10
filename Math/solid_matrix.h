622701a8fbea759cf8bc40838a75a1d9cdf23fba
gw <kuvykin@lsrm.ru>
2019-09-10 16:16:48 +0300

    added operators to solid_matrix

--- Math/solid_matrix.h
+++ Math/solid_matrix.h
@@ -17,33 +17,19 @@ public:
   Matrix(std::size_t num_rows, std::size_t num_cols) {reset(num_rows, num_cols);}
   explicit Matrix(std::size_t sze) : Matrix(sze, sze) {};
   /// Changes matrix size
-  void reset(std::size_t num_rows, std::size_t num_cols) {
-    num_rows_ = num_rows;
-    num_cols_ = num_cols;
-    data_.resize(num_rows_, num_cols_);
-  }
+  void reset(std::size_t num_rows, std::size_t num_cols);
   // access operators
   T operator()(std::size_t row, std::size_t col) const {
-    std::size_t idx = num_cols_ * row + col;
-    return data_[idx];
+    return data_[num_cols_ * row + col];
   }
   T& operator()(std::size_t row, std::size_t col) {
-    std::size_t idx = num_cols_ * row + col;
-    return data_[idx];
+    return data_[num_cols_ * row + col];
   }
   T at(std::size_t row, std::size_t col) const {
-    if (row > num_rows_ || col > num_cols_) {
-      throw std::out_of_range("index is out of range");
-    }
-    std::size_t idx = num_cols_ * row + col;
-    return data_[idx];
+    return data_.at(num_cols_ * row + col);
   }
   T& at(std::size_t row, std::size_t col) {
-    if (row > num_rows_ || col > num_cols_) {
-      throw std::out_of_range("index is out of range");
-    }
-    std::size_t idx = num_cols_ * row + col;
-    return data_[idx];
+    return data_.at(num_cols_ * row + col);
   }
   // getters
   std::size_t getRowCount() const {return num_rows_;}
@@ -54,6 +40,58 @@ private:
   std::size_t num_cols_ = 0;
   std::vector<T> data_;
 };
+
+template <typename T>
+void Matrix<T>::reset(std::size_t num_rows, std::size_t num_cols) {
+    num_rows_ = num_rows;
+    num_cols_ = num_cols;
+    data_.resize(num_rows_ * num_cols_, 0);
+}
+
+// operators
+template  <typename T>
+std::ostream& operator<<(std::ostream& out, const Matrix<T>& m) {
+  out << m.getRowCount() << " " << m.getColCount() << '\n';
+  for (std::size_t row = 0; row < m.getRowCount(); ++row) {
+    if (row > 0) {
+      out << '\n';
+    }
+    for (std::size_t col = 0; col < m.getColCount(); ++col) {
+      if (col > 0) {
+        out << ' ';
+      }
+      out << m(row, col);
+    }
+  }
+  return out;
+}
+
+template  <typename T>
+bool operator==(const Matrix<T>& lhs, const Matrix<T>& rhs) {
+  if (lhs.getRowCount() != rhs.getRowCount() || lhs.getColCount() != rhs.getColCount()) {
+    return false;
+  }
+  for (std::size_t ri = 0; ri < lhs.getRowCount(); ++ri) {
+    for (std::size_t ci = 0; ci < lhs.getColCount(); ++ci) {
+      if (lhs.at(ri, ci) != rhs.at(ri, ci)) return false;
+    }
+  }
+  return true;
+}
+
+template  <typename T>
+Matrix<T> operator+(const Matrix<T>& lhs, const Matrix<T>& rhs) {
+  if (lhs.getRowCount() != rhs.getRowCount() || lhs.getColCount() != rhs.getColCount()) {
+    throw std::invalid_argument("Exception: different matrices sizes");
+  }
+  Matrix<T> res(lhs.getRowCount(), lhs.getColCount());
+  for (int ri = 0; ri < lhs.getRowCount(); ++ri) {
+    for (int ci = 0; ci < lhs.getColCount(); ++ci) {
+      res(ri, ci) = lhs(ri, ci) + rhs(ri, ci);
+    }
+  }
+  return res;
+}
 
 
 #endif // SOLID_MATRIX_H_INCLUDED
